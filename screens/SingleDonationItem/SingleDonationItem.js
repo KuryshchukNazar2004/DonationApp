81e7f6422f6494744969f717244533e6baf67006
Nazar Kuryshchuk <kuryshchuk@Nazars-Laptop.local>
2024-09-02 22:34:30 +0300

    add route Params and Sigle Donation Item Screen

--- screens/SingleDonationItem/SingleDonationItem.js
+++ screens/SingleDonationItem/SingleDonationItem.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {Image, SafeAreaView, ScrollView, Text, View} from 'react-native';
 import BackButton from '../../components/BackButton/BackButton';
 
 import style from './style';
 import globalStyle from '../../assets/styles/globalStyle';
+import Badge from '../../components/Badge/Badge';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
-const SingleDonationItem = ({navigation}) => {
+const SingleDonationItem = ({navigation, route}) => {
   const donationItemInformation = useSelector(
     state => state.donations.selectedDonationInformation,
   );
-  console.log(donationItemInformation);
+
+  const categoryInformation = route.params.categoryInformation;
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <ScrollView
-        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
         style={style.container}>
         <BackButton onPress={() => navigation.goBack()} />
+          <Image source={{uri: donationItemInformation.image}} style={style.image} />
+          <View style={style.badge}>
+            <Badge title={categoryInformation.name} />
+          </View>
+          <Header type={1} title={donationItemInformation.name} />
+          <Text style={style.description}>
+            {donationItemInformation.description}
+          </Text>
       </ScrollView>
+      <View style={style.button}>
+        <Button title={'Donate'} />
+      </View>
     </SafeAreaView>
   );
 };
